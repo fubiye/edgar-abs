fd402d3bf586a59e4f360e2f5f6f35ab6c06c7bb
Fu, Biye <biye.fu@outlook.com>
2021-09-04 23:05:52 +0800

    enable test for all sub projects

--- edgar-utils/src/test/java/com/fubiye/edgar/utils/JsoupTest.java
+++ edgar-utils/src/test/java/com/fubiye/edgar/utils/JsoupTest.java
@@ -3,7 +3,6 @@ package com.fubiye.edgar.utils;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 
-import org.apache.commons.io.Charsets;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
@@ -13,9 +12,9 @@ public class JsoupTest {
     private static final String FILE_PATH = "D:\\data\\edgar\\sampling\\Archives\\edgar\\data\\894327\\000140720019000016\\dcent-dcmt10xdx051419.htm";
     @Test
     public void parseHtml() throws Exception{
-        File file = new File(FILE_PATH);
-        Document doc = Jsoup.parse(file, StandardCharsets.UTF_8.name());
-        String text = doc.body().text();
-        System.out.println(text);
+			File file = new File(FILE_PATH);
+			Document doc = Jsoup.parse(file, StandardCharsets.UTF_8.name());
+			String text = doc.body().text();
+			System.out.println(text);
     }    
 }
