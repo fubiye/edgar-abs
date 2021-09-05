78f733fad2fdee61e3cac8d78007d6523318b915
Fu, Biye <biye.fu@outlook.com>
2021-09-05 15:44:00 +0800

    [test] read file from test resources

--- edgar-utils/src/test/java/com/fubiye/edgar/utils/JsoupTest.java
+++ edgar-utils/src/test/java/com/fubiye/edgar/utils/JsoupTest.java
@@ -1,20 +1,20 @@
 package com.fubiye.edgar.utils;
 
-import java.io.File;
-import java.nio.charset.StandardCharsets;
-
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
 public class JsoupTest {
-    // private static final String FILE_PATH = "D:\\data\\edgar\\sampling\\Archives\\edgar\\data\\894327\\000119312515333838\\d94487d424b5.htm";
-    private static final String FILE_PATH = "D:\\data\\edgar\\sampling\\Archives\\edgar\\data\\894327\\000140720019000016\\dcent-dcmt10xdx051419.htm";
+	private static final String FILE_PATH = "edgar/data/d699698d10k.htm";
     @Test
-    public void parseHtml() throws Exception{
-			File file = new File(FILE_PATH);
+    public void parseHtml() throws Exception {
+			File file = FileUtils.toFile(this.getClass().getClassLoader().getResource(FILE_PATH));
 			Document doc = Jsoup.parse(file, StandardCharsets.UTF_8.name());
 			String text = doc.body().text();
 			System.out.println(text);
-    }    
+		}
 }
